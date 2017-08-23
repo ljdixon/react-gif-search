979f18cd19baf2f89ba3b8272346c72dfbdac6ad
Leonard Dixon <leonardjdixon@gmail.com>
2017-08-22 22:41:13 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-=======
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			gifs: [
+				{
+					id: 1,
+					url: 'http://fakeimg.pl/300/'
+				},
+				{
+					id: 2,
+					url: 'http://fakeimg.pl/300/'
+				},
+				{
+					id: 3,
+					url: 'http://fakeimg.pl/300/'
+				}
+			]
+		}
+	}
+
 	handleTermChange(term) {
 		console.log(term);
 	}
-	
+
     render() {
         return (
             <div>
 							<SearchBar onTermChange={this.handleTermChange} />
+							<GifList gifs={this.state.gifs} />
             </div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
->>>>>>> 73d04ddf4c250520c305b29193a07076fff53232
