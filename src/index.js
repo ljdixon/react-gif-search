522bedd9fa094c9cde0c2d2fc2869f4163447ff5
Leonard Dixon <ldixon@compunet.local>
2017-08-23 15:46:13 -0400

    Step 1

--- src/index.js
+++ src/index.js
@@ -1,39 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
+import GifModal from './components/GifModal';
 import SearchBar from './components/SearchBar';
+import request from 'superagent';
+import './styles/app.css';
 
 class App extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-			gifs: [
-				{
-					id: 1,
-					url: 'http://fakeimg.pl/300/'
-				},
-				{
-					id: 2,
-					url: 'http://fakeimg.pl/300/'
-				},
-				{
-					id: 3,
-					url: 'http://fakeimg.pl/300/'
-				}
-			]
-		}
+			gifs: [],
+			selectedGif: null,
+			modalIsOpen: false
+		};
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+	}
+
+	openModal(gif) {
+		this.setState({
+			modalIsOpen: true,
+			selectedGif: gif
+		});
+	}
+
+	closeModal() {
+		this.setState({
+			modalIsOpen: false,
+			selectedGif: null
+		});
 	}
 
 	handleTermChange(term) {
-		console.log(term);
+		const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+
+		request.get(url, (err, res) => {
+			this.setState({ gifs: res.body.data })
+		});
 	}
 
     render() {
         return (
             <div>
 							<SearchBar onTermChange={this.handleTermChange} />
-							<GifList gifs={this.state.gifs} />
+							<GifList gifs={this.state.gifs}
+							 				 onGifSelect={selectedGif => this.openModal(selectedGif) } />
+							<GifModal modalIsOpen={this.state.modalIsOpen}
+												selectedGif={this.state.selectedGif}
+												onRequestClose={ () => this.closeModal() } />
             </div>
         );
     }
